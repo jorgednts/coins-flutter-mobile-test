f4291359d209413cdae72e80c9012f0a67163a35
Jorge Dantas <engjorgedantas@gmail.com>
2022-04-27 16:41:16 -0300

    Create CoinListController and CoinListState

---
+++ coins_flutter_mobile_test/lib/coins/presentation/controller/coin_list_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/model/coin_model.dart';
+import '../../domain/use_case/get_coin_use_case.dart';
+import '../state/coin_list_state.dart';
+
+class CoinListController extends ValueNotifier<CoinListState> {
+  CoinListController({
+    required GetCoinUseCase getCoinUseCase,
+  })  : _getCoinUseCase = getCoinUseCase,
+        super(CoinListState.loading);
+
+  final GetCoinUseCase _getCoinUseCase;
+  CoinModel? coin;
+
+  Future<void> getCoin() async {
+    value = CoinListState.loading;
+    try {
+      coin = await _getCoinUseCase.call();
+      value = CoinListState.success;
+    } catch (e) {
+      value = CoinListState.genericError;
+    }
+  }
+}
---
+++ coins_flutter_mobile_test/lib/coins/presentation/state/coin_list_state.dart
@@ -0,0 +1,5 @@
+enum CoinListState{
+  success,
+  loading,
+  genericError,
+}
